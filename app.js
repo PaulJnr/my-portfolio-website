92e0ad23270876e391a0169bb0ea4ebdc81abe64
Pauljnr <paul.ncube14@gmail.com>
2025-04-07 08:59:06 +0200

    Task1 and Task2 submission

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const aboutMeButton = document.getElementById("#about-me-button");
-)
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
